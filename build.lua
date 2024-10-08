afcbafdc9882aa8397be0824a4a566c42e7ab160
Luiz Mugnaini <luizmugnaini@gmail.com>
2024-10-08 12:57:34 -0300

    Improve build system
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- build.lua
+++ build.lua
@@ -4,26 +4,25 @@
 -- Running the build system:  lua build.lua [options]
 -- Examples:
 -- * Single build:
---   lua build.lua build deepdive
+--   lua build.lua build
 -- * Continuous build:
---   lua build.lua watch deepdive
+--   lua build.lua watch
 
-local start_time = os.clock()
+local start_time = os.time()
 
 -- Available command line options.
 local opts = {
-    clean = false,
-    build = false,
-    watch = false,
-    -- Build the deep-dive target.
-    deepdive = false,
-    -- Build masters thesis.
-    masters = false,
+    clean = false, -- Clean previous build directory.
+    build = false, -- Perform a single build.
+    watch = false, -- Continuously build the project, watching for changes.
 }
 
 for i = 1, #arg do
     opts[arg[i]] = true
 end
+
+local os_windows = (package.config:sub(1, 1) == "\\")
+local os_info = { path_sep = os_windows and "\\" or "/" }
 
 function concat(arr)
     if #arr < 1 then
@@ -37,15 +36,59 @@ function concat(arr)
     return acc
 end
 
+function log_info(msg)
+    print("\x1b[1;35m[INFO]\x1b[0m " .. msg)
+end
+
+function log_error(msg)
+    print("\x1b[1;31m[ERROR]\x1b[0m " .. msg)
+end
+
 function exec(cmd_str)
-    print("\x1b[1;35mexecuting ::\x1b[0m " .. cmd_str)
+    log_info("executing: " .. cmd_str)
     os.execute(cmd_str)
 end
 
-local build_dir = "build"
+function concat(arr, join, is_prefix)
+    if not join then
+        join = " "
+    end
+
+    if arr == nil or #arr < 1 then
+        return ""
+    end
+
+    local acc = nil
+    if is_prefix then
+        acc = join .. arr[1]
+    else
+        acc = arr[1]
+    end
+
+    for i = 2, #arr do
+        acc = acc .. join .. arr[i]
+    end
+
+    return acc
+end
+
+function make_path(arr)
+    return concat(arr, os_info.path_sep, false)
+end
+
+function get_script_dir()
+    local info = debug.getinfo(1, "S").source:sub(2)
+    return info:match("(.*)[/\\]")
+end
+
+local root_dir = get_script_dir()
+if not root_dir then
+    root_dir = "."
+end
+
+local build_dir = make_path({ root_dir, "build" })
 local config = {
-    deep_dive_main = "deep-dive.tex",
-    masters_main = "masters.tex",
+    main = make_path({ root_dir, "src", "deep-dive.tex" }),
     builder = "latexmk",
     builder_watch_opt = "-pvc",
     builder_opts = { "-pdf", "-auxdir=" .. build_dir },
@@ -60,11 +103,15 @@ function run_builder(main_file)
 end
 
 if opts.clean then
-    os.remove(build_dir)
+    log_info("clearing build directory " .. build_dir)
+    local status = os.remove(build_dir)
+    if not status then
+        log_error("failed to delete " .. build_dir .. ", resources may be locked")
+    end
 end
 
-run_builder(config.deep_dive_main)
+run_builder(config.main)
 
 if not opts.quiet then
-    print(string.format("\x1b[1;35mtime elapsed ::\x1b[0m %.5f seconds", os.clock() - start_time))
+    log_info(string.format("time elapsed: %.5f seconds", os.time() - start_time))
 end
